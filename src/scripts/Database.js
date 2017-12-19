97c6867c1c95ad5e1469a6e931345d1317ed1377
Ben Askew <btaskew@hotmail.co.uk>
2017-12-19 09:47:08 +0000

    Extract query class

--- src/scripts/Database.js
+++ src/scripts/Database.js
@@ -4,37 +4,8 @@ import Data from 'scripts/Data';
 class Database {
     constructor() {
         this.database = new loki('recipes.db');
-    }
-
-    setup() {
         this.setRecipes();
         this.setIngredients();
-
-        console.log(this.findByIngredient('Bacon'));
-    }
-
-    findByIngredient(ingredient) {
-        const ingredients = this.ingredients.find({ingredient: ingredient});
-
-        if (!ingredients || ingredients.length < 1) {
-            return [];
-        }
-
-        return this.searchRecipes(ingredients);
-    }
-
-    searchRecipes(ingredients) {
-        const recipes = [];
-
-        for (const i in ingredients) {
-            const item = ingredients[i];
-            const recipe = this.recipes.find({id: item.recipeId});
-            if (recipe !== null && recipe.length > 0) {
-                recipes.push(recipe[0]);
-            }
-        }
-
-        return recipes;
     }
 
     setRecipes() {
@@ -48,4 +19,4 @@ class Database {
     }
 }
 
-export default new Database();
+export default Database;
