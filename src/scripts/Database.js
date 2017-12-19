ff5dcedcb5370086cf0602287a3b9913c99937d3
Ben Askew <btaskew@hotmail.co.uk>
2017-12-19 08:54:00 +0000

    Find by ingredient query setup

--- src/scripts/Database.js
+++ src/scripts/Database.js
@@ -1,11 +1,39 @@
 import loki from 'lokijs';
 
 class Database {
-    setup() {
+    constructor() {
         this.database = new loki('recipes.db');
+    }
+
+    setup() {
         this.setupRecipes();
         this.setupIngredients();
-        console.log(this.database);
+
+        console.log(this.getByIngredient('Bacon'));
+    }
+
+    getByIngredient(ingredient) {
+        const ingredients = this.ingredients.find({ingredient: ingredient});
+
+        if (!ingredients || ingredients.length < 1) {
+            return [];
+        }
+
+        return this.searchRecipes(ingredients);
+    }
+
+    searchRecipes(ingredients) {
+        const recipes = [];
+
+        for (const i in ingredients) {
+            const item = ingredients[i];
+            const recipe = this.recipes.find({id: item.recipeId});
+            if (recipe !== null && recipe.length > 0) {
+                recipes.push(recipe[0]);
+            }
+        }
+
+        return recipes;
     }
 
     setupRecipes() {
@@ -29,7 +57,8 @@ class Database {
             {id: 1, recipeId: 2, ingredient: 'Noodles'},
             {id: 1, recipeId: 3, ingredient: 'Bacon'},
             {id: 1, recipeId: 3, ingredient: 'Tomato'},
-            {id: 1, recipeId: 3, ingredient: 'Peppers'}
+            {id: 1, recipeId: 3, ingredient: 'Peppers'},
+            {id: 1, recipeId: 7, ingredient: 'foobar'}
         ]);
     }
 }
