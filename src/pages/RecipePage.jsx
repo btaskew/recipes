0636dff7bad513d90f107e76e1810f2a4c72cca9
Ben Askew <btaskew@hotmail.co.uk>
2017-12-21 13:51:06 +0000

    Fixed fake recipe link to page

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -2,24 +2,46 @@ import React, {Component} from 'react';
 
 import Recipe from 'scripts/Recipe';
 
+import {Alert} from 'react-bootstrap';
 import Page from 'components/utils/Page';
 import IngredientsList from 'components/IngredientsList';
 
 class RecipePage extends Component {
     constructor(props) {
         super(props);
-        this.state = {ingredients: null};
+        this.state = {
+            ingredients: null,
+            heading: 'Loading...',
+            message: null
+        };
     }
 
     componentDidMount() {
         const recipeId = parseInt(this.props.match.params.id, 10);
+        const recipe = Recipe.findById(recipeId);
+        if (!recipe || recipe.length < 1) {
+            this.showError();
+            return;
+        }
         const ingredients = Recipe.relatedIngredients(recipeId);
-        this.setState({ingredients});
+        this.setState({ingredients, heading: recipe[0].name});
+    }
+
+    showError() {
+        this.setState({
+            message: (
+                <Alert bsStyle="danger">
+                    Error loading recipe, please try again later
+                </Alert>
+            ),
+            heading: "Error"
+        });
     }
 
     render() {
         return (
-            <Page heading={this.props.match.params.name} homeIcon>
+            <Page heading={this.state.heading} homeIcon>
+                {this.state.message}
                 {this.state.ingredients && (
                     <IngredientsList ingredients={this.state.ingredients} />
                 )}
