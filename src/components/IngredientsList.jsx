6fbb5add4e1e8d5ffff4a6ab2d4aa2b9e727b56f
Ben Askew <btaskew@hotmail.co.uk>
2017-12-31 09:35:10 +0000

    Rework IngredientsList to display message if no ingredients found

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,21 +1,24 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 
+import {Alert} from 'react-bootstrap';
 import Ingredient from 'components/Ingredient';
 
-class IngredientsList extends PureComponent {
-    render() {
-        return (
-            <div className="flex">
-                {this.props.ingredients.map(ingredient => (
-                    <Ingredient
-                        key={ingredient.$loki}
-                        id={ingredient.$loki}
-                        name={ingredient.name}
-                    />
-                ))}
-            </div>
-        );
+function IngredientsList(props) {
+    if(props.ingredients.length < 1) {
+        return <Alert bsStyle="info">No ingredients found</Alert>;
     }
+
+    return (
+        <div className="flex">
+            {props.ingredients.map(ingredient => (
+                <Ingredient
+                    key={ingredient.$loki}
+                    id={ingredient.$loki}
+                    name={ingredient.name}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default IngredientsList;
