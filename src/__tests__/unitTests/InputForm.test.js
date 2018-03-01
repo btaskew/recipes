f6868d3288be6621d77f7bcc5c50010def417111
Ben Askew <btaskew@hotmail.co.uk>
2018-03-01 09:41:29 +0000

    Complete InputForm test

--- src/__tests__/unitTests/InputForm.test.js
+++ src/__tests__/unitTests/InputForm.test.js
@@ -6,6 +6,14 @@ import InputForm from 'components/InputForm';
 import {Alert} from 'react-bootstrap';
 
 describe('InputForm', () => {
+    it('should display the passed in placeholder and button text', () => {
+        const wrapper = mount(
+            <InputForm inputText="Test holder" buttonText="Test button" submitForm={() => {}} />
+        );
+
+        expect(wrapper.find('input').props().placeholder).toBe('Test holder');
+        expect(wrapper.find('button').props().children).toBe('Test button');
+    });
 
     it('should display the value stored in state', () => {
         const wrapper = mount(<InputForm submitForm={() => {}} />);
@@ -35,16 +43,19 @@ describe('InputForm', () => {
     });
 
     describe('handleSubmit', () => {
-        it('should set noValue when submitted with no input', () => {
-            const wrapper = shallow(<InputForm submitForm={() => {}} />);
+        let wrapper;
 
+        beforeEach(() => {
+            wrapper = shallow(<InputForm submitForm={() => {}} />);
+        });
+
+        it('should set noValue when submitted with no input', () => {
             wrapper.instance().handleSubmit();
 
             expect(wrapper.state('noValue')).toBeTruthy();
         });
 
         it('should reset the state when submitted with valid input', () => {
-            const wrapper = shallow(<InputForm submitForm={() => {}} />);
             wrapper.setState({value: 'Test'});
 
             wrapper.instance().handleSubmit();
@@ -53,20 +64,42 @@ describe('InputForm', () => {
             expect(wrapper.state('value')).toBe('');
         });
 
-        it('should call props.submitForm when submitted with valid input', () => {
+        it('should call props.submitForm with valid input', () => {
             const mockProps = {
                 submitForm: () => {}
             };
             const spy = jest.spyOn(mockProps, 'submitForm');
 
-            const wrapper = shallow(<InputForm submitForm={mockProps.submitForm} />);
+            wrapper = shallow(<InputForm submitForm={mockProps.submitForm} />);
 
             wrapper.setState({value: 'Test'});
-
             wrapper.instance().handleSubmit();
+
+            expect(spy).toHaveBeenCalledWith('Test');
+
+            spy.mockClear();
+        });
+    });
+
+    describe('User input', () => {
+        it('should call handleChange when the input is changed', () => {
+            const spy = jest.spyOn(InputForm.prototype, 'handleChange');
+
+            const wrapper = mount(<InputForm submitForm={() => {}} />);
+
+            wrapper.find('input').simulate('change', {target: {value: 'Test'}});
+
+            expect(spy).toHaveBeenCalled();
+        });
+
+        it('should call handleSubmit when the button is clicked', () => {
+            const spy = jest.spyOn(InputForm.prototype, 'handleSubmit');
+
+            const wrapper = mount(<InputForm submitForm={() => {}} />);
+
+            wrapper.find('button').simulate('click');
 
             expect(spy).toHaveBeenCalled();
         });
     });
-
 });
