00fbb4bf6e07be032fdda3ea7f3b97899e6d2109
baskew <b.askew@exeter.ac.uk>
2019-05-10 09:56:42 +0100

    Update packages

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
     modulePaths: ['<rootDir>/src/'],
     collectCoverage: true,
     collectCoverageFrom: ['**/*.jsx', '!**/node_modules/**'],
+    testURL: 'http://localhost'
 };
